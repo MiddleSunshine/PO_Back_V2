2b7a3f5ab9a5f08da08956fb65c59d84a901bf6d
yangqingxian <qingxianyang@gmail.com>
2023-02-16 02:58:47 -0800

    增加控制器

---
+++ basic/controllers/WhiteBordController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\controllers;
+
+class WhiteBordController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
