69e74f35f91b3e508861352314f09eb258bf50d4
yangqingxian <qingxianyang@gmail.com>
2023-02-16 02:58:15 -0800

    增加控制器

---
+++ basic/controllers/PointsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\controllers;
+
+class PointsController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
